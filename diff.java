7f54101e83e990353c90fffad0b10d98e32291ac
Mayank Rusia <mayankrusia97@gmail.com>
2023-09-11 21:32:18 +0530

    new commit

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String[] args)
+    {
+        int a=50;
+        int b=20;
+        System.out.println("Difference between a & b is: "+(a-b));
+    }
+    
+}
