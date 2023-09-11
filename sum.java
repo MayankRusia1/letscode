388fa54492473089a4d1ab4ab5c4a1979fc3188a
Mayank Rusia <mayankrusia97@gmail.com>
2023-09-11 21:52:29 +0530

    fact file

--- sum.java
+++ sum.java
@@ -4,7 +4,6 @@ public class sum
     {
         int a=10;
         int b=20;
-        int c=a+b;
-        System.out.println("Sum of "+a+" and "+b+" is: "+c);
+        System.out.println("Sum of "+a+" and "+b+" is: "+(a+b));
     }
 }
